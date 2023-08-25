3e0515aac5ed69b04367b1943d04db0a254a99e0
Thiago de Matos <thiago.dematos.1@gmail.com>
2023-08-25 08:06:53 -0400

    started creating logic in js file to add marks onto board

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-function Gameboard() {
+//create a gamboard using the module pattern
+const Gameboard = (() => {
   let gameBoardArray = [];
-}
+})();
 
-function Player() {
+//create a function for creating a player object
+function Player(name) {
   this.name = name;
 }
 
-function GameControls() {}
+//create a function for playing the game using the module pattern
+const GameControls = (() => {})();
+
+function addXMarks() {
+  const p = document.createElement("p");
+  const xMark = (document.querySelector("p").innerText = "X");
+
+  selectBox.addeventlistener("click", function () {});
+}
+
+//selecting all of the gameboard boxes
+//set html class to the same name
+//select class and push objects into the array
